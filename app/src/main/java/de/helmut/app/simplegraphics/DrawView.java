58b3e7282431f826c4aa9a64d51318586ac9e3a7
Prof. Dr. Helmut Roderus <helmut.roderus@hs-ansbach.de>
2018-12-18 09:03:35 +0100

    Erweiterung von DrawView um OnTouch-Event. Der Kreis wird jetzt dort gezeichnet, wo dieses Event registriert wurde.

--- app/src/main/java/de/helmut/app/simplegraphics/DrawView.java
+++ app/src/main/java/de/helmut/app/simplegraphics/DrawView.java
@@ -14,17 +14,25 @@ import android.view.View.OnTouchListener;
  * DrawView enthält die Zeichenfunktionalität und ist eine View-Subklasse.
  * Das Zeichnen wird durch Aufruf der Methode onDraw ausgelöst.
  */
-public class DrawView extends View  {
+public class DrawView extends View implements OnTouchListener {
 
     private static final String TAG = "DrawView";
 
     // Die Eigenschaften des gezeichneten Objekts werden in einem Paint-Objekt gespeichert
     private Paint myPaint;
 
+    // Die geometrischen Eigenschaften des Kreisobjekts
+    private float mx = 300;
+    private float my = 500;
+    private float radius = 200;
+
     public DrawView(Context context) {
         super(context);
         setFocusable(true);
         setFocusableInTouchMode(true);
+
+        // Dieses Instanz stellt auch den Eventhandler für das OnTouch-Event bereit (s.u.)
+        this.setOnTouchListener(this);
 
         // Eine Instanz von Paint erzeugen, Farbe auf Rot setzen, Antialiasing einschalten.
         myPaint = new Paint();
@@ -37,8 +45,22 @@ public class DrawView extends View  {
     */
     @Override
     public void onDraw(Canvas canvas) {
-        // Zeichne ein Kreis mit x=300, y=500, radius=200 und den myPaint-Eigenschaften
-        canvas.drawCircle(300, 500,200, myPaint);
+        // Zeichne einen Kreis
+        canvas.drawCircle(mx, my, radius, myPaint);
+    }
+
+    public boolean onTouch(View view, MotionEvent event) {
+
+        // Die Koordinaten des Touch-Events holen und als Kreismittelpunkt speichern
+        mx = event.getX();
+        my = event.getY();
+
+        /* Die Zeichenfläche wird nur dann gezeichnet, wenn es notwendig ist. Der Aufruf von
+           invalidate() teilt dem Android-System mit, dass dies der Fall ist.
+        */
+        // invalidate();
+
+        return true;
     }
 }
 
